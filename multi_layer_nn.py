23d77a36727672c3c9558dc906aabe8c02b31bf3
Ben <maiahee@gmail.com>
2018-10-11 23:06:05 -0700

    training works?
    sigmoid has overflows, may be an issue

--- multi_layer_nn.py
+++ multi_layer_nn.py
@@ -5,6 +5,8 @@ import math
 
 HIDDEN_LAYER_SIZE = 20
 OUTPUT_COUNT = 10
+LEARNING_RATE = .01
+MOMENTUM = .9
 
 def get_dataset_from_file(path):
     with open(path) as dataset_file:
@@ -25,44 +27,68 @@ def get_dataset_from_file(path):
         tagged_set = [tags, data_set]
     return tagged_set
 
+# compute the accuracy of the given perceptrons at predicting the given inputs
+def compute_accuracy(dataset, perceptrons):
+    count = correct = 0
+    for img in dataset:
+        if img[0] == guess_didgit(perceptrons, img):
+            correct += 1
+        count += 1
+    return correct / count
+
+# generate random weights for a step in the NN, height is inputs, width is nodes
+def generate_weight_matrix(height, width):
+    matrix = []
+    for row in range(0, height):
+        matrix.append([])
+        for col in range(0, width):
+            matrix[row].append(random.uniform(-.05, .05))
+    return np.asmatrix(matrix)
+
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     training_data_path = argv.pop()
     test_data_path = argv.pop()
     training_data = get_dataset_from_file(training_data_path)
-    #test_data = get_dataset_from_file(test_data_path)
+    ######################test_data = get_dataset_from_file(test_data_path)
     print('Finished Importing data')
     
     # initialize
     input_size = len(training_data[1][0])
-    #input_size = training_data[1].shape[1]
-    hidden_layer_weights = []
-    for input in range(0, input_size):
-        hidden_layer_weights.append([])
-        for node in range(0, HIDDEN_LAYER_SIZE):
-            hidden_layer_weights[input].append(random.uniform(-.05, .05))
-    hidden_layer_weights = np.asmatrix(hidden_layer_weights)
+    hidden_layer_weights = generate_weight_matrix(input_size, HIDDEN_LAYER_SIZE)
+    output_weights = generate_weight_matrix(HIDDEN_LAYER_SIZE, OUTPUT_COUNT)
 
-    output_weights = []
-    for hidden_node in range(0, HIDDEN_LAYER_SIZE):
-        output_weights.append([])
-        for output in range(0, OUTPUT_COUNT):
-            output_weights[hidden_node].append(random.uniform(-.05, .05))
-    output_weights = np.asmatrix(output_weights)
+    def sigmoid(x): 
+        return 1 / (1 + np.exp(-x))
+    vsigmoid = np.vectorize(sigmoid)
 
-    sigmoid = np.vectorize(lambda x: 1 / (1 + (math.e ** x)))
-    targets = np.identity(OUTPUT_COUNT) * .9
+    targets = (np.ones((10,10)) * .1) + (np.identity(OUTPUT_COUNT) * .8)
 
     # begin training
-    for epoch in range(0,1):
+    previous_OWD = 0
+    previous_HWD = 0
+    for epoch in range(0,50):
         for tag, input in zip(training_data[0], training_data[1]):
             hidden_nodes = input * hidden_layer_weights
-            sigmoid(hidden_nodes)
+            vsigmoid(hidden_nodes)
 
             outputs = hidden_nodes * output_weights
-            sigmoid(outputs)
-            print(outputs)
+            vsigmoid(outputs)
+
+            target = targets[tag,:]
+            output_errors = np.multiply(np.multiply(outputs, (1 - outputs)), np.subtract(target, outputs))
+            hidden_errors = np.multiply(np.multiply(hidden_nodes, (1 - hidden_nodes)), np.sum((output_weights * output_errors.T), axis=0))
+            
+            output_weight_deltas = ((LEARNING_RATE * output_errors).T * hidden_nodes).T + (MOMENTUM * previous_OWD)
+            previous_OWD = output_weight_deltas
+            output_weights = output_weights + output_weight_deltas
+            
+            hidden_weight_deltas = ((LEARNING_RATE * hidden_errors).T * input).T + (MOMENTUM * previous_HWD)
+            previous_HWD = hidden_weight_deltas
+            hidden_layer_weights = hidden_layer_weights + hidden_weight_deltas
+        print('Epoch ' + str(epoch))
+            
 
 
     #hidden_layer = training_data[1] * hidden_layer_weights
